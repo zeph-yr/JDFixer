b37cbec0405ea3010550d8789790a3ac22782d92
Zephyr <67527746+zeph-yr@users.noreply.github.com>
2022-02-23 21:13:17 +0800

    Fix to coexist with CC and base campaign

--- Managers/JDFixerUIManager.cs
+++ Managers/JDFixerUIManager.cs
@@ -77,61 +77,68 @@ namespace JDFixer.Managers
             // If a map is not dled, missionID and beatmapDifficulty will be correct, but beatmapCharacteristic will be null
             // Accessing any null values of arg1 or arg2 will crash CC horribly
 
-            if (Plugin.cc_installed && arg2.missionData != null && arg2.missionData.beatmapCharacteristic != null)
+            if (Plugin.cc_installed)
             {
-                Logger.log.Debug("In CC, MissionNode exists");
-
-                Logger.log.Debug("MissionNode - missionid: " + arg2.missionId); //"<color=#0a92ea>[STND]</color> Holdin' Oneb28Easy-1"
-                Logger.log.Debug("MissionNode - difficulty: " + arg2.missionData.beatmapDifficulty); // "Easy" etc
-                Logger.log.Debug("MissionNode - characteristic: " + arg2.missionData.beatmapCharacteristic.serializedName); //"Standard" etc
-
-                /*
-                // Exploring what's in here:
-                Logger.log.Debug("MissionNode - name: " + arg2.name); // "MissionNode has name MissionNode_1(Clone)" for everything
-                Logger.log.Debug("MissionNode - letter: " + arg2.letterPartName); // Empty
-                Logger.log.Debug("MissionNode - number: " + arg2.numberPartName); // -1 for everything
-                Logger.log.Debug("MissionNode - formatted name: " + arg2.formattedMissionNodeName); // Empty
-                Logger.log.Debug("MissionNode - tag: " + arg2.tag); // "Untagged"
-
-                Logger.log.Debug("MissionNode - name: " + arg2.missionData.name); // Empty
-                Logger.log.Debug("MissionNode - objectives: " + arg2.missionData.missionObjectives[0].ToString()); // Seems always Empty
-                Logger.log.Debug("MissionNode - missionhelp: " + arg2.missionData.missionHelp); // "(CustomCampaigns.Campaign.Missions.CustomMissionHelpSO)" // Probably not useful
-                Logger.log.Debug("MissionNode - duration: " + arg2.missionData.level.songDuration); // Crashes everytime, Seems there is no level in CC
-                */
-
-                /*
-                [DEBUG @ 21:40:52 | JDFixer] MissionNode exists
-                [DEBUG @ 21:40:52 | JDFixer] MissionNode - missionid: < color =#c27ef2> Wildcard1d3bcHard-1
-                [DEBUG @ 21:40:52 | JDFixer] MissionNode - difficulty: Hard
-                [DEBUG @ 21:40:52 | JDFixer] MissionNode - characteristic: Standard
-                [DEBUG @ 21:40:52 | JDFixer] CC Level: custom_level_35B125930B0F475431AFCFF0362711D98CFEEAA6
-                */
-
-                if (MissionSelectionPatch.cc_level != null) // lol null check just to print?
+                if (arg2.missionData != null && arg2.missionData.beatmapCharacteristic != null)
                 {
-                    // If a map is not dled, this will be the previous selected node's map
-                    // Possible edge case where CC util will find a match in the previous selected node's map using the current node's diff and characteristic?
-                    // but does it really matter if the map isnt dl and the user isnt playing it? Nope lol
+                    Logger.log.Debug("In CC, MissionNode exists");
 
-                    Logger.log.Debug("CC Level: " + MissionSelectionPatch.cc_level.levelID);  // For cross check with arg2.missionId
+                    Logger.log.Debug("MissionNode - missionid: " + arg2.missionId); //"<color=#0a92ea>[STND]</color> Holdin' Oneb28Easy-1"
+                    Logger.log.Debug("MissionNode - difficulty: " + arg2.missionData.beatmapDifficulty); // "Easy" etc
+                    Logger.log.Debug("MissionNode - characteristic: " + arg2.missionData.beatmapCharacteristic.serializedName); //"Standard" etc
 
-                    IDifficultyBeatmap difficulty_beatmap = CustomCampaigns.Utils.BeatmapUtils.GetMatchingBeatmapDifficulty(MissionSelectionPatch.cc_level.levelID, arg2.missionData.beatmapCharacteristic, arg2.missionData.beatmapDifficulty);
+                    /*
+                    // Exploring what's in here:
+                    Logger.log.Debug("MissionNode - name: " + arg2.name); // "MissionNode has name MissionNode_1(Clone)" for everything
+                    Logger.log.Debug("MissionNode - letter: " + arg2.letterPartName); // Empty
+                    Logger.log.Debug("MissionNode - number: " + arg2.numberPartName); // -1 for everything
+                    Logger.log.Debug("MissionNode - formatted name: " + arg2.formattedMissionNodeName); // Empty
+                    Logger.log.Debug("MissionNode - tag: " + arg2.tag); // "Untagged"
 
-                    if (difficulty_beatmap != null) // lol null check just to print?
+                    Logger.log.Debug("MissionNode - name: " + arg2.missionData.name); // Empty
+                    Logger.log.Debug("MissionNode - objectives: " + arg2.missionData.missionObjectives[0].ToString()); // Seems always Empty
+                    Logger.log.Debug("MissionNode - missionhelp: " + arg2.missionData.missionHelp); // "(CustomCampaigns.Campaign.Missions.CustomMissionHelpSO)" // Probably not useful
+                    Logger.log.Debug("MissionNode - duration: " + arg2.missionData.level.songDuration); // Crashes everytime, Seems there is no level in CC
+                    */
+
+                    /*
+                    [DEBUG @ 21:40:52 | JDFixer] MissionNode exists
+                    [DEBUG @ 21:40:52 | JDFixer] MissionNode - missionid: < color =#c27ef2> Wildcard1d3bcHard-1
+                    [DEBUG @ 21:40:52 | JDFixer] MissionNode - difficulty: Hard
+                    [DEBUG @ 21:40:52 | JDFixer] MissionNode - characteristic: Standard
+                    [DEBUG @ 21:40:52 | JDFixer] CC Level: custom_level_35B125930B0F475431AFCFF0362711D98CFEEAA6
+                    */
+
+                    if (MissionSelectionPatch.cc_level != null) // lol null check just to print?
                     {
-                        Logger.log.Debug("MissionNode Diff: " + difficulty_beatmap.difficulty);  // For cross check with arg2.missionData.beatmapDifficulty
-                        Logger.log.Debug("MissionNode Offset: " + difficulty_beatmap.noteJumpStartBeatOffset);
-                        Logger.log.Debug("MissionNode NJS: " + difficulty_beatmap.noteJumpMovementSpeed);
+                        // If a map is not dled, this will be the previous selected node's map
+                        // Possible edge case where CC util will find a match in the previous selected node's map using the current node's diff and characteristic?
+                        // but does it really matter if the map isnt dl and the user isnt playing it? Nope lol
 
-                        DiffcultyBeatmapUpdated(difficulty_beatmap);
+                        Logger.log.Debug("CC Level: " + MissionSelectionPatch.cc_level.levelID);  // For cross check with arg2.missionId
+
+                        IDifficultyBeatmap difficulty_beatmap = CustomCampaigns.Utils.BeatmapUtils.GetMatchingBeatmapDifficulty(MissionSelectionPatch.cc_level.levelID, arg2.missionData.beatmapCharacteristic, arg2.missionData.beatmapDifficulty);
+
+                        if (difficulty_beatmap != null) // lol null check just to print?
+                        {
+                            Logger.log.Debug("MissionNode Diff: " + difficulty_beatmap.difficulty);  // For cross check with arg2.missionData.beatmapDifficulty
+                            Logger.log.Debug("MissionNode Offset: " + difficulty_beatmap.noteJumpStartBeatOffset);
+                            Logger.log.Debug("MissionNode NJS: " + difficulty_beatmap.noteJumpMovementSpeed);
+
+                            DiffcultyBeatmapUpdated(difficulty_beatmap);
+                        }
                     }
+                }
+                else // Map not dled
+                {
+                    DiffcultyBeatmapUpdated(null);
                 }
             }
 
-            // QOL: When map is not dled or in Base Campaigns, set Map JD and Reaction Time displays to show zeroes to prevent misleading player
-            else
+            // Base campaign
+            else if (arg2 != null)
             {
-                DiffcultyBeatmapUpdated(null);
+                DiffcultyBeatmapUpdated(arg2.missionData.level.GetDifficultyBeatmap(arg2.missionData.beatmapCharacteristic, arg2.missionData.beatmapDifficulty));
             }
         }
 
