2cb6c2406312f2dfdd6f274e489270cc393f70b1
HalloTheEngineer <spacecitymc@outlook.de>
2025-05-11 21:32:10 +0200

    not added to git?!

---
+++ UI/JDPreferencesListViewController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HMUI;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.ViewControllers;
+using BeatSaberMarkupLanguage.Components.Settings;
+using System.ComponentModel;
+
+namespace JDFixer.UI
+{
+    internal sealed class JDPreferencesListViewController : BSMLResourceViewController, INotifyPropertyChanged
+    {
+        public override string ResourceName => "JDFixer.UI.BSML.preferencesList.bsml";
+
+
+        [UIComponent("njs_slider")] private SliderSetting NJS_Slider;
+
+        private float New_NJS_Value = 16f;
+
+        [UIValue("njs_value")]
+        private float NJS_Value
+        {
+            get => New_NJS_Value;
+            set { New_NJS_Value = value; }
+        }
+
+        [UIAction("set_njs_value")]
+        private void Set_NJS_Value(float value)
+        {
+            NJS_Value = value;
+        }
+
+
+        [UIValue("min_jd_slider")] private float Min_JD_Slider => PluginConfig.Instance.minJumpDistance;
+        [UIValue("max_jd_slider")] private float Max_JD_Slider => PluginConfig.Instance.maxJumpDistance;
+
+        [UIComponent("jd_slider")] private SliderSetting JD_Slider;
+
+        private float New_JD_Value = 18f;
+
+        [UIValue("jd_value")]
+        private float JD_Value
+        {
+            get => New_JD_Value;
+            set => New_JD_Value = value;
+        }
+
+        [UIAction("set_jd_value")]
+        private void Set_JD_Value(float value)
+        {
+            JD_Value = value;
+        }
+
+
+        [UIComponent("pref_list")] private CustomListTableData Pref_List;
+        private JDPref Selected_Pref = null;
+
+
+        [UIAction("select_pref")]
+        private void Select_Pref(TableView tableView, int row)
+        {
+            Selected_Pref = PluginConfig.Instance.preferredValues_jd[PluginConfig.Instance.use_jd_pref][row];
+        }
+
+
+        [UIAction("add_pressed")]
+        private void Add_Pressed()
+        {
+            if (PluginConfig.Instance.preferredValues_jd[PluginConfig.Instance.use_jd_pref]
+                .Any(x => x.njs == New_NJS_Value))
+            {
+                PluginConfig.Instance.preferredValues_jd[PluginConfig.Instance.use_jd_pref]
+                    .RemoveAll(x => x.njs == New_NJS_Value);
+            }
+
+            PluginConfig.Instance.preferredValues_jd[PluginConfig.Instance.use_jd_pref]
+                .Add(new JDPref(New_NJS_Value, New_JD_Value));
+            Reload_List_From_Config();
+        }
+
+
+        [UIAction("remove_pressed")]
+        private void Remove_Pressed()
+        {
+            if (Selected_Pref == null)
+            {
+                return;
+            }
+
+            PluginConfig.Instance.preferredValues_jd[PluginConfig.Instance.use_jd_pref]
+                .RemoveAll(x => x == Selected_Pref);
+            Reload_List_From_Config();
+        }
+
+
+        private void Reload_List_From_Config()
+        {
+            Pref_List.Data.Clear();
+
+            var index = PluginConfig.Instance.use_jd_pref;
+            
+            if (PluginConfig.Instance.preferredValues_jd == null)
+                return;
+            
+            // First element to start with
+            if (PluginConfig.Instance.preferredValues_jd.Count == 0)
+            {
+                PluginConfig.Instance.preferredValues_jd.Add(new List<JDPref>());
+            }
+
+            // Handle out of bounds config
+            if (PluginConfig.Instance.preferredValues_jd.Count <= index)
+            {
+                PluginConfig.Instance.use_rt_pref = PluginConfig.Instance.preferredValues_jd.Count-1;
+            }
+
+
+            PluginConfig.Instance.preferredValues_jd[index]
+                .Sort((x, y) => y.njs.CompareTo(x.njs));
+
+            foreach (var pref in PluginConfig.Instance.preferredValues_jd[index])
+            {
+                Pref_List.Data.Add(
+                    new CustomListTableData.CustomCellInfo($"{pref.njs} NJS | {pref.jumpDistance} Jump Distance"));
+            }
+
+            Pref_List.TableView.ReloadData();
+            Pref_List.TableView.ClearSelection();
+            Selected_Pref = null;
+        }
+
+
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
+            if (!firstActivation)
+            {
+                Reload_List_From_Config();
+            }
+        }
+
+        protected override void DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling)
+        {
+            base.DidDeactivate(removedFromHierarchy, screenSystemDisabling);
+        }
+
+
+        [UIAction("#post-parse")]
+        private void PostParse()
+        {
+            Reload_List_From_Config();
+        }
+    }
+}
